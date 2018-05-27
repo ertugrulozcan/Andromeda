1b01cdec2fb539d84d45225cc2568b98b2a7bb55
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2018-05-27 23:22:26 +0300

    AppInstallerListener added

---
+++ app/src/main/java/com/ertis/andromeda/listeners/AppInstallerListener.java
@@ -0,0 +1,14 @@
+package com.ertis.andromeda.listeners;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AppInstallerListener extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+	
+	}
+}
