5926912f44c6bfcafc35934e1eea1519402e484d
MATRIKS\ertugrulo <ertugrul.ozcan@matriksdata.com>
2019-01-09 20:43:39 +0300

    Update Colors.java

--- app/src/main/java/com/aero/andromeda/helpers/Colors.java
+++ app/src/main/java/com/aero/andromeda/helpers/Colors.java
@@ -1,5 +1,6 @@
 package com.aero.andromeda.helpers;
 
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 
 public class Colors
@@ -39,6 +40,25 @@ public class Colors
 		}
 	}
 	
+	public static int getAlpha(ColorDrawable drawable)
+	{
+		return getAlpha(ToArgbString(drawable));
+	}
+	
+	public static int getAlpha(String argb)
+	{
+		if (!argb.startsWith("#"))
+			argb = "#" + argb;
+		
+		if (argb.length() == 7)
+			return 255;
+		
+		argb = argb.replaceFirst("^#", "");
+		String alphaHex = argb.substring(0, 2);
+		
+		return Integer.parseInt(alphaHex, 16);
+	}
+	
 	public static ColorDrawable clearAlpha(ColorDrawable drawable)
 	{
 		try
@@ -51,5 +71,60 @@ public class Colors
 		{
 			return drawable;
 		}
+	}
+	
+	public static ColorDrawable ManipulateOpacity(ColorDrawable drawable, int alpha)
+	{
+		int color = drawable.getColor();
+		
+		int red = Color.red(color);
+		int blue = Color.blue(color);
+		int green = Color.green(color);
+		
+		String a = Integer.toHexString(alpha);
+		if (a.length() == 1)
+			a = "0" + a;
+		
+		String r = Integer.toHexString(red);
+		if (r.length() == 1)
+			r = "0" + r;
+		
+		String g = Integer.toHexString(green);
+		if (g.length() == 1)
+			g = "0" + g;
+		
+		String b = Integer.toHexString(blue);
+		if (b.length() == 1)
+			b = "0" + b;
+		
+		return rgb("#" + a + r + g + b);
+	}
+	
+	public static String ToArgbString(ColorDrawable drawable)
+	{
+		int color = drawable.getColor();
+		
+		int red = Color.red(color);
+		int blue = Color.blue(color);
+		int green = Color.green(color);
+		int alpha = Color.alpha(color);
+		
+		String a = Integer.toHexString(alpha);
+		if (a.length() == 1)
+			a = "0" + a;
+		
+		String r = Integer.toHexString(red);
+		if (r.length() == 1)
+			r = "0" + r;
+		
+		String g = Integer.toHexString(green);
+		if (g.length() == 1)
+			g = "0" + g;
+		
+		String b = Integer.toHexString(blue);
+		if (b.length() == 1)
+			b = "0" + b;
+		
+		return "#" + a + r + g + b;
 	}
 }
